63c96bf232cc6363c88829471683d787a284e080
Nikolay Zinov <nzinov@yandex-team.ru>
2019-12-25 13:12:30 +0300

    add stlxxl

--- stxxl/main.cpp
+++ stxxl/main.cpp
@@ -8,27 +8,25 @@
 
 #include <sstream>
 
-typedef long long int64;
-
 struct MyInt
 {
-    int64 key_;
+    uint64_t key_;
 
-    int64 key() const
+    uint64_t key() const
     {
         return key_;
     }
 
     MyInt() { }
-    MyInt(int64 k): key_(k) { }
+    MyInt(uint64_t k): key_(k) { }
 
-    static int64 min_value()
+    static uint64_t min_value()
     {
-        return std::numeric_limits<int64>::min();
+        return std::numeric_limits<uint64_t>::min();
     }
-    static int64 max_value()
+    static uint64_t max_value()
     {
-        return std::numeric_limits<int64>::max();
+        return std::numeric_limits<uint64_t>::max();
     }
 };
 
@@ -69,31 +67,38 @@ std::ostream& operator << (std::ostream& o, const MyInt& obj)
 
 int main(int argc, char** argv)
 {
-    if (argc != 2)
+    if (argc != 4)
     {
-        // WRITE_CODE_HERE: consider output and memory limit here.
-        std::cout << "Usage: " << argv[0] << " <in>" << std::endl;
+        std::cout << "Usage: " << argv[0] << " <in> <out> <memoryLimit>" << std::endl;
         return 1;
     }
+
+    const stxxl::internal_size_type memoryLimit = atoi(argv[3]);
+    const stxxl::internal_size_type blockSize = 4096;
 
     // Disable cpu parallelism.
     omp_set_num_threads(1);
 
     stxxl::syscall_file fin(argv[1], stxxl::file::DIRECT | stxxl::file::RDONLY);
+    stxxl::syscall_file fout(argv[2], stxxl::file::DIRECT | stxxl::file::RDWR | stxxl::file::CREAT);
 
     // Block size is hardcoded for HDD.
     const stxxl::unsigned_type block_size = 8 * 1024 * 1024;
     typedef stxxl::vector<MyInt, 1, stxxl::lru_pager<8>, block_size> vector_type;
 
     vector_type input(&fin);
+    vector_type output(&fout);
+    output.resize(input.size());
 
     typedef stxxl::stream::streamify_traits<vector_type::iterator>::stream_type input_stream_type;
     input_stream_type input_stream = stxxl::stream::streamify(input.begin(), input.end());
 
-    // WRITE_CODE_HERE: call stxxl sorting.
-
     typedef Cmp comparator_type;
-    STXXL_MSG((stxxl::is_sorted(input.begin(), input.end(), comparator_type()) ? "OK" : "WRONG"));
+    typedef stxxl::stream::sort<input_stream_type, Cmp, blockSize> sort_stream_type;
+    sort_stream_type sort_stream(input_stream, comparator_type(), memoryLimit);
+    vector_type::iterator o = stxxl::stream::materialize(sort_stream, output.begin(), output.end());
+
+    STXXL_MSG((stxxl::is_sorted(output.begin(), output.end(), comparator_type()) ? "OK" : "WRONG"));
 
     return 0;
 }
